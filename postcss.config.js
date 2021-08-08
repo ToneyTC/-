d5260a3e450f5f69b052a2670edcd4bb5011d835
liushuhao126 <liushuhao126@126.com>
2021-08-09 01:19:49 +0800

    feat 作业

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-//根目录新增的postcss.config.js
-module.exports = {
-  plugins: [
-    // postcss插件
-    require('postcss-preset-env')()
-  ]
-}
--- src/js/common.js
+++
@@ -1,4 +0,0 @@
-require('../css/commonCSS/normalize.css')
-require('../css/commonCSS/property.css')
-require('../css/commonCSS/reset.css')
-require('../css/commonCSS/bisck.less')
